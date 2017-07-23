25bcac536e29a8dbab58aca8bae88f6a6f8f8893
Guilherme Dias <gldias97@gmail.com>
2017-07-23 15:09:11 -0400

    Added Toolbar widget to Main Activity (and removed the use of the deprecated native action bar)

--- app/src/main/java/com/hugehard/sharables/MainActivity.java
+++ app/src/main/java/com/hugehard/sharables/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,9 +19,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         List<Recipe> recipeList = new ArrayList<>();
 
+        //Test recipes
         String testTitle = "Bananas";
         String testAuthor = "Gui";
         int testTime = 20;
@@ -36,6 +40,31 @@ public class MainActivity extends AppCompatActivity {
 
         Recipe testRecipe = new Recipe(testTitle, testAuthor, testTime, testIngredients, testPreparation, testSteps);
         recipeList.add(testRecipe);
+
+        Recipe testRecipe2 = new Recipe("Apples", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe2);
+
+        Recipe testRecipe3 = new Recipe("Oranges", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe3);
+
+        Recipe testRecipe4 = new Recipe("PineapplesAndMorePineapples", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe4);
+
+        Recipe testRecipe5 = new Recipe("Mangoes", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe5);
+
+        Recipe testRecipe6 = new Recipe("Cherries", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe6);
+
+        Recipe testRecipe7 = new Recipe("Clementines", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe7);
+
+        Recipe testRecipe8 = new Recipe("Strawberries", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe8);
+
+        Recipe testRecipe9 = new Recipe("Blueberries", testAuthor, testTime, testIngredients, testPreparation, testSteps);
+        recipeList.add(testRecipe9);
+        //End test recipes
 
         ListView listView = (ListView)findViewById(R.id.list);
         RecipeListAdapter recipeListAdapter = new RecipeListAdapter(this, R.layout.mylist, recipeList);
