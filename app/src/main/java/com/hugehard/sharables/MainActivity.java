63124a477245ed59fafca7621e811dbc82fe3807
Guilherme Dias <gldias97@gmail.com>
2017-07-26 19:57:51 -0400

    Added toolbar overflow menu to main activity

--- app/src/main/java/com/hugehard/sharables/MainActivity.java
+++ app/src/main/java/com/hugehard/sharables/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -104,6 +105,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
 }
