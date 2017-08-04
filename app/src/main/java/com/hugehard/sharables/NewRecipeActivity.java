06e812aca3eda16cca789234fd739c2d35377509
Guilherme Dias <gldias97@gmail.com>
2017-08-03 20:07:08 -0400

    Continued building new recipe form. Add New Ingredient buttons kinda works, but the positioning of the new input fields is off. Will fix later.

--- app/src/main/java/com/hugehard/sharables/NewRecipeActivity.java
+++ app/src/main/java/com/hugehard/sharables/NewRecipeActivity.java
@@ -7,8 +7,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class NewRecipeActivity extends AppCompatActivity {
+
+    private LinearLayout ingredientsView;
+    private Button addNewIngredientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +37,43 @@ public class NewRecipeActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ingredientsView = (LinearLayout)findViewById(R.id.recipe_ingredients_wrapper);
+        addNewIngredientButton = (Button) findViewById(R.id.add_new_ingredient_button);
+        addNewIngredientButton.setOnClickListener(onClick());
+    }
+
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addIngredientInputLine();
+            }
+        };
+    }
+
+    private void addIngredientInputLine() {
+        LinearLayout ingredientInput = (LinearLayout)findViewById(R.id.recipe_ingredients_input);
+
+        LinearLayout newLine = new LinearLayout(this);
+        EditText ingredientName = new EditText(this);
+        EditText ingredientQuantity = new EditText(this);
+
+        LinearLayout.LayoutParams linearLayout = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams ingName = new LinearLayout.LayoutParams(192, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams ingQuant = new LinearLayout.LayoutParams(148, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        newLine.setLayoutParams(linearLayout);
+        newLine.setOrientation(LinearLayout.HORIZONTAL);
+
+        ingredientName.setLayoutParams(ingName);
+        ingredientName.setHint("Ingredient Name");
+        newLine.addView(ingredientName);
+
+        ingredientQuantity.setLayoutParams(ingQuant);
+        ingredientQuantity.setHint("Quantity");
+        newLine.addView(ingredientQuantity);
+
+        ingredientInput.addView(newLine);
     }
 }
